31ac1bcdea5921ffb0c12f9e604bb44f878680fd
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-09-26 20:58:12 -0300

    initial commit

---
+++ src/main/java/br/com/vfs/word/information/colector/WordInformationColectorApplication.java
@@ -0,0 +1,13 @@
+package br.com.vfs.word.information.colector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordInformationColectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordInformationColectorApplication.class, args);
+	}
+
+}
