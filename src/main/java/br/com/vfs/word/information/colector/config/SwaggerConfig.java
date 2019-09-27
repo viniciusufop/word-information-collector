ff1635191e828c3bde322b40f132df64b3bb20e2
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-09-26 21:17:43 -0300

    incluindo swagger, lombok,  primeiro controller e biblioteca jsoup no projeto

---
+++ src/main/java/br/com/vfs/word/information/colector/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package br.com.vfs.word.information.colector.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket greetingApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .build()
+                .apiInfo(metaData());
+
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("\"Spring Boot REST API for greeting people\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .build();
+    }
+}
---
+++ src/main/java/br/com/vfs/word/information/colector/controller/ColectorController.java
@@ -0,0 +1,21 @@
+package br.com.vfs.word.information.colector.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("colector")
+public class ColectorController {
+
+    @PostMapping("/actuator")
+    public ResponseEntity actuator() {
+        log.info("m=actuator, atualizando dicionario");
+
+
+        return ResponseEntity.ok().build();
+    }
+}
