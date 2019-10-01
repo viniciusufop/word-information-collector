24a273a4489e1bacd6c4f734ba0b7075a784e1e9
Vinicius Faria de Souza <tqi_vfaria@uolinc.com>
2019-10-01 09:22:50 -0300

    habilitar o async para o spring boot

--- word-information-collector-back/src/main/java/br/com/vfs/word/information/collector/WordInformationCollectorApplication.java
+++ word-information-collector-back/src/main/java/br/com/vfs/word/information/collector/WordInformationCollectorApplication.java
@@ -2,12 +2,14 @@ package br.com.vfs.word.information.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class WordInformationCollectorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WordInformationCollectorApplication.class, args);
-	}
+    public static void main(String[] args){
+        SpringApplication.run(WordInformationCollectorApplication.class, args);
+    }
 
 }
